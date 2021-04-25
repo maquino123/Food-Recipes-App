38b19c4c31faa4ef22b6ef7e331a5e090b24b173
maquino123 <marlon.aquino24@gmail.com>
2021-04-25 01:16:40 -0700

    Basecode for food recipe app
    Added a basic splash screen that include a progress bar

---
+++ app/src/main/java/com/bignerdranch/android/food_recipes_app/HomeScreen.kt
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.food_recipes_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
