625429a5b375f7fcb1b01428cf9a36e65868eab3
maquino123 <marlon.aquino24@gmail.com>
2021-05-06 00:08:03 -0700

    add dish names layout file

--- app/src/main/java/com/bignerdranch/android/food_recipes_app/GetStartedScreen.kt
+++ app/src/main/java/com/bignerdranch/android/food_recipes_app/GetStartedScreen.kt
@@ -1,11 +1,20 @@
 package com.bignerdranch.android.food_recipes_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_getstarted.*
 
 class GetStartedScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_getstarted)
+
+        getStartedButton.setOnClickListener{
+            var intent = Intent(this@GetStartedScreen, HomeScreen::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
---
+++ app/src/main/java/com/bignerdranch/android/food_recipes_app/HomeScreen.kt
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.food_recipes_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+
+class HomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+    }
+}
